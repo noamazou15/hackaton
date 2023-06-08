ce86ac85b9daf076a750938b6718e889338abbca
noamazou15 <noamazou15@gmail.com>
2023-06-09 00:09:49 +0300

    wdwd

--- namespaces/places.py
+++ namespaces/places.py
@@ -7,7 +7,7 @@ from pymongo.server_api import ServerApi
 
 from bson import ObjectId
 import json
-from utils import convert_objectid_to_str
+from utils import convert_objectid_to_str, is_within_radius
 
 
 uri = "mongodb+srv://root:root@cluster0.npjz8p7.mongodb.net/"
@@ -30,8 +30,24 @@ class Places(Resource):
 class Place(Resource):
     def get(self):
         payload = ns.payload
-        location = payload.get('id')
-        place = mongo_client.packages.find({"pickup_location": location })
-        place = convert_objectid_to_str(list(place))
-        return place
+        lon = payload.get('lon')
+        lat = payload.get('lat')
+        radius = payload.get('radius')
+        location = ObjectId(payload.get('id'))
+        locations = mongo_client.locations.find({"pickup_center": False})
+        filtered_locs = []
+        for loc in locations:
+            place_lat = loc['coordinates']['latitude']
+            place_lon = loc['coordinates']['longitude']
+            print(is_within_radius(radius, lat, lon, place_lat, place_lon))
+            if is_within_radius(radius, lat, lon, place_lat, place_lon):
+                filtered_locs.append(loc['_id'])
+        
+        place_cursor = mongo_client.packages.find({"pickup_location": location,"dropoff_location": {"$in": filtered_locs} })
+        place_cursor = list(place_cursor)
+        place_cursor = convert_objectid_to_str(place_cursor)
+
+        return place_cursor
+    
+
     

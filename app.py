b3550e40eefcdb53d92c7f0fa4a9a6fd7c089eae
noamazou15 <noamazou15@gmail.com>
2023-06-08 15:56:58 +0300

    namespace

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ from flask_restx import Api
 from flask import session
 from flask_restx import Resource, fields, abort
 from flask_restx.namespace import Namespace
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+
+
+import namespaces
 
 uri = "localhost:27017"
 # Create a new client and connect to the server
@@ -35,25 +39,7 @@ api = AppApi(version="1.0", title="My API", description="Platform", prefix='/api
 
 api.init_app(app, add_specs=ENABLE_SWAGGER)  # add_specs False to disable UI and Swagger.json
 
-
-
-ns_packages = Namespace("Packages", description="packages managment", path='/')
-api.add_namespace(ns_packages)
-
-@ns_packages.route('/new_package')
-class Login(Resource):
-    def post(self):
-        payload = ns_packages.payload
-        pickup = payload.get('pickup')
-        dropoff = payload.get('dropoff')
-        packageID = payload.get('packageID')
-        notes = payload.get('extraNotes')
-        user_session = session.get('user_session')
-        user_id = mongo_client.users.find_one({'user_session': user_session})['_id']
-        mongo_client.packages.insert_one({'pickup_location': pickup, 'dropoff_location': dropoff,
-                '_id': packageID, 'notes': notes,'user_session': user_id})
-
-        return user_session
+api.add_namespace(namespaces.packages.ns_packages)
 
 
 
